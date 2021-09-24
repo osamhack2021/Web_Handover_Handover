cc04fcb426cb2f44265b7130f8ff49840c549336
오지환 <50487467+134130@users.noreply.github.com>
2021-09-24 09:05:14 +0000

    Style: Combining Error Class files
    Combined Error Class files based on Parent Class.
    
    Related to: #40

--- backend/services/errors/BussinessError.js
+++
@@ -1,9 +0,0 @@
-class BussinessError extends Error {
-    constructor(message) {
-        super(message);
-        this.status = 400;
-        this.name = "BussinessError";
-    }
-}
-
-module.exports = BussinessError;
