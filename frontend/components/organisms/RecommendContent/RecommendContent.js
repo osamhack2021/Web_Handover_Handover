e3d042337742efce086428d8ed53fd69e76f8f9e
Max Kim <50355670+holymollyhao@users.noreply.github.com>
2021-09-26 07:38:36 +0000

    Feat : Add Horizontal scrolling to recommend card Added horizontal scrolling, s.t. upon scrolling inside recommendation pages scrolls horizontally
    Resolves :  #33

--- frontend/components/organisms/RecommendContent/RecommendContent.js
+++ frontend/components/organisms/RecommendContent/RecommendContent.js
@@ -1,7 +1,8 @@
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Card from '_molecules/Card';
 import PropTypes from 'prop-types';
+import HorizontalScroll from 'react-scroll-horizontal';
 
 function ArrayToComponent(type, cardArray) {
   return (cardArray.map(
@@ -19,13 +20,34 @@ function ArrayToComponent(type, cardArray) {
 
 export default function RecommendContent({ type, cardArray }) {
   // const arrCard = ArrayToComponent(type, cardArray);
+  // const recommendCards = useRef();
+
+  const recommendCard = useRef();
+  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    const container = recommendCard.current;
+    if (container) {
+      const onWheel = (e) => {
+        if (e.deltaY === 0) return;
+        e.preventDefault();
+        container.scrollTo({
+          left: container.scrollLeft + e.deltaY,
+          behavior: 'smooth',
+        });
+      };
+      container.addEventListener('wheel', onWheel);
+      return () => container.removeEventListener('wheel', onWheel);
+    }
+  }, []);
+
   return (
-    <div className="recommend-card">
+    <div className="recommend-card" ref={recommendCard}>
       <Card type={type} />
       <Card type={type} />
       <Card type={type} />
       <Card type={type} />
     </div>
+
   );
 }
 
