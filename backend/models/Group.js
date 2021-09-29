94fc5282ea91d3cd0ecfa33bbb9fba2e8cdce88d
오지환 <50487467+134130@users.noreply.github.com>
2021-09-29 13:36:58 +0000

    Feat: Unique Array for admins field

--- backend/models/Group.js
+++ backend/models/Group.js
@@ -3,21 +3,46 @@ const Types = mongoose.Schema.Types
 
 const groupSchema = mongoose.Schema({
     name: { type: String, required: true },
-    path: { type: String, required: true, unique: true },
+    path: { type: String, required: true },
     admins: {
-        type: [{ type: Types.ObjectId, ref: 'User' }],
+        type: [{ type: Types.ObjectId, ref: 'User', populate: true }],
         required: true
     },
-    inspectors: [{ type: Types.ObjectId, ref: 'User' }]
+    inspectors: [{ type: Types.ObjectId, ref: 'User', populate: true }]
 }, {
     versionKey: false
 });
 
-groupSchema.statics.create = function(payload) {
-    if(payload.admins.length > 1) {
-        payload.admins = payload.admins.map(objectId => mongoose.Types.ObjectId(objectId));
-    }
+function distinctObjectIdArray(arr) {
 
+    /* Removing duplicate values */
+
+    // ["61507eaab51b4983b5fb8f1a", "61507eaab51b4983b5fb8f1a"]
+    let uniqueArr = arr.map(item => item.toString());
+
+    // ["61507eaab51b4983b5fb8f1a"]
+    uniqueArr = [...new Set(arr)];
+
+    // [ new ObjectId("61507eaab51b4983b5fb8f1a") ]
+    uniqueArr = arr.map(item => mongoose.Types.ObjectId(item));
+
+    return uniqueArr;
+}
+
+groupSchema.pre('save', function(next) {
+    this.admins = distinctObjectIdArray(this.arr);
+    next();
+});
+groupSchema.pre('updateOne', function(next) {
+    const data = this.getUpdate();
+
+    data.admins = distinctObjectIdArray(data.admins);
+    this.update({}, data);
+
+    next();
+});
+
+groupSchema.statics.create = function(payload) {
     const group = new this(payload);
 
     return group.save();
