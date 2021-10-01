b52f82d836558829d70559f619ca3ad5fef226e3
오지환 <50487467+134130@users.noreply.github.com>
2021-10-01 14:25:31 +0000

    Update InitDB.js

--- backend/InitDB.js
+++ backend/InitDB.js
@@ -1,94 +1,8 @@
-const mongoose = require('mongoose');
+const { Types } = require('mongoose');
 
 const User = require('./models/User.js');
 const Group = require('./models/Group.js');
 const Item = require('./models/Item.js');
-
-let users = [{
-    serviceNumber: 'ntcho',
-    password: 'ntcho',
-    name: '조나단',
-    rank: '병장',
-    title: '팀장',
-    status: 'active'
-}, {
-    serviceNumber: 'phjppo0918',
-    password: 'phjppo0918',
-    name: '박현준',
-    rank: '상병',
-    title: '백엔드 개발자',
-    status: 'active'
-}, {
-    serviceNumber: '134130',
-    password: '134130',
-    name: '오지환',
-    rank: '일병',
-    title: '백엔드 노예',
-    status: 'active'
-}, {
-    serviceNumber: 'ahnavocado',
-    password: 'ahnavocado',
-    name: '안수겸',
-    rank: '상병',
-    title: '디자이너',
-    status: 'active'
-}, {
-    serviceNumber: 'cw00h',
-    password: 'cw00h',
-    name: '최우혁',
-    rank: '일병',
-    title: '프론트 개발자',
-    status: 'active'
-}, {
-    serviceNumber: 'holymollyhao',
-    password: 'holymollyhao',
-    name: '김태원',
-    rank: '일병',
-    title: '프론트 개발자',
-    status: 'active'
-}];
-
-let groups = [{
-    name: 'handover',
-    path: ',handover,',
-}, {
-    name: 'backend',
-    path: ',handover,backend,',
-}, {
-    name: 'frontend',
-    path: ',handover,frontend,',
-}];
-
-let items = [{
-    title: 'Handover',
-    type: 'cabinet',
-    owner: mongoose.Types.ObjectId(),
-    path: ',Handover,',
-    content: 'Handover Cabinet',
-    status: 'published'
-}, {
-    title: 'handover',
-    type: 'document',
-    owner: mongoose.Types.ObjectId(),
-    path: ',Handover,handover,',
-    content: 'handover document',
-    status: 'published'
-}, {
-    title: 'backend',
-    type: 'card',
-    owner: mongoose.Types.ObjectId(),
-    path: ',Handover,handover,backend,',
-    content: 'backend card',
-    status: 'published'
-}, {
-    title: 'frontend',
-    type: 'card',
-    owner: mongoose.Types.ObjectId(),
-    path: ',Handover,handover,frontend,',
-    content: 'frontend card',
-    status: 'deleted'
-}
-];
 
 const crypto = require('crypto');
 
@@ -99,97 +13,172 @@ function encode(rowPassword) {
 }
 
 async function init() {
+    const USERS = [{
+        serviceNumber: 'ntcho',
+        password: 'ntcho',
+        name: '조나단',
+        rank: '병장',
+        title: '팀장',
+        status: 'admin',
+    }, {
+        serviceNumber: 'phjppo0918',
+        password: 'phjppo0918',
+        name: '박현준',
+        rank: '상병',
+        title: '백엔드 개발자',
+        status: 'active',
+    }, {
+        serviceNumber: '134130',
+        password: '134130',
+        name: '오지환',
+        rank: '일병',
+        title: '백엔드 노예',
+        status: 'active'
+    }, {
+        serviceNumber: 'ahnavocado',
+        password: 'ahnavocado',
+        name: '안수겸',
+        rank: '상병',
+        title: '디자이너',
+        status: 'active'
+    }, {
+        serviceNumber: 'cw00h',
+        password: 'cw00h',
+        name: '최우혁',
+        rank: '일병',
+        title: '프론트 개발자',
+        status: 'active'
+    }, {
+        serviceNumber: 'holymollyhao',
+        password: 'holymollyhao',
+        name: '김태원',
+        rank: '일병',
+        title: '프론트 개발자',
+        status: 'active'
+    }];
+    
+    const GROUPS = [{
+        name: 'handover',
+        path: ',handover,',
+        admins: 'ntcho',
+        inspectors: []
+    }, {
+        name: 'backend',
+        path: ',handover,backend,',
+        admins: 'phjppo0918',
+        inspectors: []
+    }, {
+        name: 'frontend',
+        path: ',handover,frontend,',
+        admins: 'cw00h',
+        inspectors: []
+    }, {
+        name: 'design',
+        path: ',handover,frontend,design,',
+        admins: 'ahnavocado',
+        inspectors: []
+    }];
+    
+    const ITEMS = [{
+        title: 'Handover',
+        type: 'cabinet',
+        owner: 'ntcho',
+        path: ',Handover,',
+        content: 'Handover Cabinet',
+        tags: ['Handover'],
+        accessGroups: {
+            read: [],
+            edit: []
+        },
+        status: 'published'
+    }, {
+        title: 'handover',
+        type: 'document',
+        owner: 'ntcho',
+        path: ',Handover,handover,',
+        content: 'handover document',
+        tags: ['Handover'],
+        accessGroups: {
+            read: [],
+            edit: []
+        },
+        status: 'published'
+    }, {
+        title: 'backend',
+        type: 'card',
+        owner: 'phjppo0918',
+        path: ',Handover,handover,backend,',
+        content: 'backend card',
+        tags: ['Handover'],
+        accessGroups: {
+            read: [],
+            edit: []
+        },
+        status: 'published'
+    }, {
+        title: 'frontend',
+        type: 'card',
+        owner: 'cw00h',
+        path: ',Handover,handover,frontend,',
+        content: 'frontend card',
+        tags: ['Handover'],
+        accessGroups: {
+            read: [],
+            edit: []
+        },
+        status: 'published'
+    }];
+    
+    let Users = {};
+    let Groups = {};
+    let Items = {};
+
     try {
-        await Group.collection.drop();
-        await User.collection.drop();
-        await Item.collection.drop();
 
-        await createGroups();
-        await createUsers();
-        await createItems();
-
-        let ntcho = (await User.find({ serviceNumber: users[0].serviceNumber }))[0];
-        await Group.updateOne(groups[0], { admins: [ mongoose.Types.ObjectId(ntcho._id) ] });
-        await Item.updateOne(items[0], {
-            owner: mongoose.Types.ObjectId(ntcho._id),
-            accessGroups: {
-                read: [mongoose.Types.ObjectId(ntcho.group)],
-                edit: [mongoose.Types.ObjectId(ntcho.group)]
-            }
-        });
-        await Item.updateOne(items[1], {
-            owner: mongoose.Types.ObjectId(ntcho._id),
-            accessGroups: {
-                read: [mongoose.Types.ObjectId(ntcho.group)],
-                edit: [mongoose.Types.ObjectId(ntcho.group)]
-            }
-        });
-
-        let phjppo0918 = (await User.find({ serviceNumber: users[1].serviceNumber }))[0];
-        await Group.updateOne(groups[1], { admins: [ mongoose.Types.ObjectId(phjppo0918._id) ] });
-        await Item.updateOne(items[2], {
-            owner: mongoose.Types.ObjectId(phjppo0918._id),
-            accessGroups: {
-                read: [mongoose.Types.ObjectId(phjppo0918.group)],
-                edit: [mongoose.Types.ObjectId(phjppo0918.group)]
-            }
-        });
-
-        let ahnavocado = (await User.find({ serviceNumber: users[3].serviceNumber }))[0];
-        await Group.updateOne(groups[2], { admins: [ mongoose.Types.ObjectId(ahnavocado._id) ] });
-        await Item.updateOne(items[3], {
-            owner: mongoose.Types.ObjectId(ahnavocado._id),
-            accessGroups: {
-                read: [mongoose.Types.ObjectId(ahnavocado.group)],
-                edit: [mongoose.Types.ObjectId(ahnavocado.group)]
-            }
-        });
+        // Reset DB
+        await Promise.all([
+            User.collection.drop(),
+            Group.collection.drop(),
+            Item.collection.drop()
+        ]);
     } catch(e) {
         console.log(e);
-    }
-}
+    } finally {
 
-async function createUsers() {
-    let handoverId = (await Group.find({ name: 'handover' }))[0]._id;
-    let backendId = (await Group.find({ name: 'backend' }))[0]._id;
-    let frontendId = (await Group.find({ name: 'frontend' }))[0]._id;
-
-    users[0].group = mongoose.Types.ObjectId(handoverId);
-    users[1].group = mongoose.Types.ObjectId(backendId);
-    users[2].group = mongoose.Types.ObjectId(backendId);
-    users[3].group = mongoose.Types.ObjectId(frontendId);
-    users[4].group = mongoose.Types.ObjectId(frontendId);
-    users[5].group = mongoose.Types.ObjectId(frontendId);
-
-    for(let user of users) {
-        user.password = encode(user.serviceNumber);
-        let user_ = new User(user);
-        await user_.save();
-    }
-}
-
-async function createGroups() {
-    try {
-        for(let group of groups) {
-            let group_ = new Group(group);
-            await group_.save();
+        // Create Users
+        for(let user of USERS) {
+            user.password = encode(user.password);
+            Users[user.serviceNumber] = await User.create(user);
         }
-    } catch(e) {
-        console.error(e);
-    }
-}
 
-
-
-async function createItems() {
-    try {
-        for(let item of items) {
-            let item_ = new Item(item);
-            await item_.save();
+        // Create Groups
+        for(let group of GROUPS) {
+            group.admins = [Types.ObjectId(Users[group.admins]._id)];
+            Groups[group.name] = await Group.create(group);
         }
-    } catch(e) {
-        console.error(e);
+
+        // Set Users group
+        for(let name in Groups) {
+            let admin = Groups[name].admins[0];
+            Users[admin.serviceNumber] = await User.updateByid(admin, { group: Groups[name]._id });
+        }
+
+        // Create Items
+        for(let item of ITEMS) {
+            let user = Users[item.owner];
+
+            user = await User.findOne({ _id: user._id });
+
+            item.owner = Types.ObjectId(user._id);
+            item.accessGroups = {
+                read: [user.group],
+                edit: [user.group]
+            };
+            Items[item.title] = await Item.create(item);
+        }
     }
 }
+
+init();
 
 module.exports = { init };
