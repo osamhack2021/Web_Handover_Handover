c98bf6f522f4c14a267dbf072013e75edf63a196
오지환 <50487467+134130@users.noreply.github.com>
2021-10-01 14:33:01 +0000

    Fix: Error Handler can resolve unhandled error.

--- backend/app.js
+++ backend/app.js
@@ -51,5 +51,5 @@ app.use(function(req, res, next) {
 });
 
 app.use(function(error, req, res, next) {
-	res.status(error.status).send(error.message);
+	res.status(error.status || 500).send(error.message);
 });
