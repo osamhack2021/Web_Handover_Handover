6e30afb8d7697676f9a4fe80776723c8d43577ff
Woo hyeok Choi <cwooh8@gmail.com>
2021-10-02 05:30:45 +0000

    Feat: Add subrouter to SettingPage
    Added subrouter to SettingPage
    
    Resolves: #33

--- frontend/components/pages/SettingsPage/SettingsPage.js
+++ frontend/components/pages/SettingsPage/SettingsPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Switch, Route } from 'react-router';
+
 import ProfileSettings from '_organisms/ProfileSettings';
 import Drawer from '_organisms/Drawer';
+import Profile from '_organisms/Profile';
 
 import User from '_assets/svgs/user.svg';
 
@@ -8,8 +11,8 @@ export default function SettingsPage() {
   const menulist = [
     'ME',
     [
-      {name: '프로필', src: User, alt: 'user', link: '/'},
-      {name: '테마 변경', src: User, alt: 'themechange', link: '/'},
+      {name: '프로필', src: User, alt: 'user', link: '/setting/profile'},
+      {name: '테마 변경', src: User, alt: 'themechange', link: '/setting/themechange'},
     ],
     'TEAM',
     [
@@ -23,7 +26,10 @@ export default function SettingsPage() {
         <Drawer name="야옹이" rank="이병" title="짬타이거" division="0사단 00연대" menulist={menulist} />
       </div>
       <div className="setting-page">
-        <ProfileSettings />
+        <Switch>
+          {/* <Route path="/setting/themechange" component={} /> */}
+          <Route path="/setting" component={ProfileSettings} />
+        </Switch>
       </div>
     </div>
   )
