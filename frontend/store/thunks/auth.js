c4f27c7c444b3139239103e6293bd9bd5f67d87d
Woo hyeok Choi <cwooh8@gmail.com>
2021-10-03 06:58:28 +0000

    Fix: Revert merge deletion

--- frontend/store/thunks/auth.js
+++ frontend/store/thunks/auth.js
@@ -1,38 +1,78 @@
-import { push } from 'connected-react-router';
-import { camelToSnakeCase, snakeToCamelCase } from 'json-style-converter/es5';
-import { store as RNC } from 'react-notifications-component';
+import {
+  push
+} from 'connected-react-router';
+import {
+  camelToSnakeCase,
+  snakeToCamelCase
+} from 'json-style-converter/es5';
+import {
+  store as RNC
+} from 'react-notifications-component';
 
-import { postRegister, postLogin, postLogout } from '_api/auth';
-import { login, logout } from '_actions/user';
-import { dispatchError, handleSuccess } from '_utils/api';
+import {
+  postRegister,
+  postLogin,
+  postLogout
+} from '_api/auth';
+import {
+  login,
+  logout
+} from '_actions/user';
+import {
+  dispatchError,
+  handleSuccess
+} from '_utils/api';
 import R from 'ramda';
 
-import { attemptGetGroup } from './group';
-import { attemptLoadItems } from './item';
+import {
+  attemptGetGroup
+} from './group';
+import {
+  attemptLoadItems
+} from './item';
 
 export const attemptLogin = user => dispatch =>
   postLogin(user)
-    .then(data => {
-      dispatch(login(snakeToCamelCase(data)));
+  .then(data => {
+    dispatch(login(snakeToCamelCase(data)));
 
-      console.log(`Login successful: response is ${JSON.stringify(data)}`)
+    console.log(`Login successful: response is ${JSON.stringify(data)}`)
 
-      RNC.addNotification({
-        title: 'Success!',
-        message: `JWT token = ${data.token}`, // TODO: change to confirmation message
-        type: 'success',
-        container: 'top-right',
-        animationIn: ['animated', 'fadeInRight'],
-        animationOut: ['animated', 'fadeOutRight'],
-        dismiss: {
-          duration: 5000,
-        },
-      });
+    RNC.addNotification({
+      title: 'Success!',
+      message: `JWT token = ${data.token}`, // TODO: change to confirmation message
+      type: 'success',
+      container: 'top-right',
+      animationIn: ['animated', 'fadeInRight'],
+      animationOut: ['animated', 'fadeOutRight'],
+      dismiss: {
+        duration: 5000,
+      },
+    });
 
-      dispatch(push('/'));
-      return data;
-    })
-    .catch(dispatchError(dispatch));
+    dispatch(push('/'));
+    return data;
+  })
+  .catch(dispatchError(dispatch));
+
+export const attemptRegister = (newUser) => (dispatch) => postRegister(newUser)
+  .then((data) => {
+    RNC.addNotification({
+      title: 'Success!',
+      message: data.message,
+      type: 'success',
+      container: 'top-right',
+      animationIn: ['animated', 'fadeInRight'],
+      animationOut: ['animated', 'fadeOutRight'],
+      dismiss: {
+        duration: 5000,
+      },
+    });
+
+    return dispatch(attemptLogin(newUser));
+  })
+  .then(() => dispatch(push('/settings')))
+  .catch(dispatchError(dispatch));
 
 export const attemptLogout = () => (dispatch) => postLogout()
   .then((data) => {
