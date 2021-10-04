954dfdc6905eb9304a806fefe2aeeb3e14377455
Nathan Cho <ntcho13@gmail.com>
2021-10-04 06:24:26 +0000

    Feat: Site-wide theme with mui ThemeProvider
    The primary and secondary colors are just temporary for now

--- frontend/components/environment/Root/Root.js
+++ frontend/components/environment/Root/Root.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router';
+import React from "react";
+import PropTypes from "prop-types";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 
-import Main from '_environment/Main';
+import Main from "_environment/Main";
+
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+});
 
 export default function Root({ history, store }) {
   return (
     <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <Main />
-      </ConnectedRouter>
+      <ThemeProvider theme={theme}>
+        <ConnectedRouter history={history}>
+          <Main />
+        </ConnectedRouter>
+      </ThemeProvider>
     </Provider>
   );
 }
