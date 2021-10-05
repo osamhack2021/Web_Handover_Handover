46e9aa31ad6e696a316c1778a22105b0d4f7c2a0
Woo hyeok Choi <cwooh8@gmail.com>
2021-10-05 02:40:00 +0000

    Fix: Change GroupMember's layout
    Modify GroupMember to have same layout regardless of length of the text
    
    Resolves: #33

--- frontend/components/organisms/GroupSettings/GroupSettings.js
+++ frontend/components/organisms/GroupSettings/GroupSettings.js
@@ -23,12 +23,19 @@ export default function ProfileSettings() {
       </Box>
       <TabPanel value={value} index={0} className="tabpanel">
         <div className="tabpanel-title"> 그룹원 </div>
-        <GroupMember name="최우혁" rank="일병" division="31사단 95여단" title="TOD 운용병" status="활동중" admin="그룹원" />
-        <GroupMember name="조나단" rank="병장" division="1사단 22여단" title="팀장" status="활동중" admin="관리자" />
+        <GroupMember name="최우혁" rank="일병" division="31사단 95여단" title="대형차량운용병" status="active" admin="admin" />
+        <GroupMember name="조나단" rank="병장" division="1사단 22여단" title="팀장" status="active" admin="관리자" />
         <GroupMember name="김태원" rank="상병" division="2사단 33여단" title="프론트엔드" status="비활성" admin="그룹원" />
-        <GroupMember name="오지환" rank="이병" division="3사단 44여단" title="백엔드" status="활동중" admin="그룹원" />
-        <GroupMember name="안수겸" rank="상병" division="4사단 55여단" title="디자인" status="활동중" admin="그룹원" />
-        <GroupMember name="박현준" rank="병장" division="5사단 66여단" title="백엔드" status="비활성" admin="관리자" />
+        <GroupMember name="오지환" rank="이병" division="3사단 44여단" title="백엔드" status="active" admin="그룹원" />
+        <GroupMember name="안수겸" rank="상병" division="4사단 55여단" title="디자인" status="active" admin="그룹원" />
+        <GroupMember name="박현준" rank="병장" division="5사단 66여단" title="백엔드" status="active" admin="admin" />
+        <GroupMember name="박현준" rank="병장" division="5사단 66여단" title="백엔드" status="active" admin="admin" />
+        <GroupMember name="박현준" rank="병장" division="5사단 66여단" title="백엔드" status="active" admin="admin" />
+        <GroupMember name="박현준" rank="병장" division="5사단 66여단" title="백엔드" status="active" admin="admin" />
+        <GroupMember name="박현준" rank="병장" division="5사단 66여단" title="백엔드" status="active" admin="admin" />
+        <GroupMember name="박현준" rank="병장" division="5사단 66여단" title="백엔드" status="active" admin="admin" />
+        <GroupMember name="박현준" rank="병장" division="5사단 66여단" title="백엔드" status="active" admin="admin" />
+
       </TabPanel>
       <TabPanel value={value} index={1} className="tabpanel">
         2
