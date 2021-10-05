75bda47ee2cd229fc7406d0962893c5e294564bb
오지환 <50487467+134130@users.noreply.github.com>
2021-10-05 13:36:05 +0000

    Feat: Add Algolia
    Algolia enables delivering relevant content.
    https://www.algolia.com/
    
    Related to: #137

---
+++ backend/algolia/index.js
@@ -0,0 +1,10 @@
+const algoliasearch = require('algoliasearch');
+
+const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID;
+const ALGOLIA_ADMIN_API_KEY = process.env.ALGOLIA_ADMIN_API_KEY;
+const ALGOLIA_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME;
+
+const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_API_KEY);
+const index = client.initIndex(ALGOLIA_INDEX_NAME);
+
+module.exports = index;
