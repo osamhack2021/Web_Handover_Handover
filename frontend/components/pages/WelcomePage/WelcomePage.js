2f7e1409add8b4edbb2791d2f66e277dbdb28706
Nathan Cho <ntcho13@gmail.com>
2021-10-10 21:02:00 +0900

    Design: Update WelcomePage with Container

--- frontend/components/pages/WelcomePage/WelcomePage.js
+++ frontend/components/pages/WelcomePage/WelcomePage.js
@@ -1,25 +1,26 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { push } from 'connected-react-router';
-import R from 'ramda';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { push } from "connected-react-router";
+import R from "ramda";
 
-import Container from '@mui/material/Container';
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
 
 export default function WelcomePage() {
   const dispatch = useDispatch();
-  const { user } = useSelector(R.pick(['user']));
+  const { user } = useSelector(R.pick(["user"]));
 
   useEffect(() => {
     if (!R.isEmpty(user)) {
-      dispatch(push('/home'));
+      dispatch(push("/home"));
     }
   }, []);
 
   return (
-    <div className="welcome-page page">
-      <Container>
-        Welcome!
-      </Container>
-    </div>
+    <Container maxWidth="md" sx={{ height: "100%" }}>
+      <Stack>
+        Landing Page
+      </Stack>
+    </Container>
   );
 }
