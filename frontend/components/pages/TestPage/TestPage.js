d9523277ced87a47f366a55d69d08221f3c32e94
Nathan Cho <ntcho13@gmail.com>
2021-10-11 19:39:42 +0900

    Fix: Fix TestPage crashing with index error

--- frontend/components/pages/TestPage/TestPage.js
+++ frontend/components/pages/TestPage/TestPage.js
@@ -6,17 +6,12 @@ import { attemptLogout } from '_thunks/auth';
 
 export default function TestPage() {
   const { userItem } = useSelector(R.pick(['userItem']));
-  const dispatch = useDispatch();
-  const currentId = userItem[0].Id;
+  
   const render = userItem.map((elem) => <Card Id={elem.Id} />);
-
-  const logout = () => {
-    dispatch(attemptLogout());
-  };
+  
   return (
     <div>
-      <Card Id={userItem[2].Id} />
-      <Card Id={userItem[3].Id} />
+      {render}
     </div>
   );
 }
