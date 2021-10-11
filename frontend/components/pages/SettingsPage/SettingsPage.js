8f3f19211444dee51ca7445620bfadf4adba8b76
Woo hyeok Choi <cwooh8@gmail.com>
2021-10-11 09:07:42 +0000

    Fix: Change link to GroupSettings
    Resolves: #33

--- frontend/components/pages/SettingsPage/SettingsPage.js
+++ frontend/components/pages/SettingsPage/SettingsPage.js
@@ -11,30 +11,30 @@ import User from '_assets/svgs/user.svg';
 
 export default function SettingsPage() {
   const { user } = useSelector(R.pick(['user']));
+
   const menulist = [
     'ME',
     [
-      {title: '프로필', src: User, alt: 'user', link: '/account/settings'},
-      {title: '테마 변경', src: User, alt: 'themechange', link: '/account/settings/theme'},
+      { title: '프로필', src: User, alt: 'user', link: '/account/settings' },
+      { title: '테마 변경', src: User, alt: 'themechange', link: '/account/settings/theme' },
     ],
     'TEAM',
     [
-      {title: '그룹 관리', src: User, alt: 'auth', link: '/account/settings/groups'},
-      {title: '알림', src: User, alt: 'alert', link: '/account/settings/alerts'},
+      { title: '그룹 관리', src: User, alt: 'auth', link: '/account/settings/group' },
+      { title: '알림', src: User, alt: 'alert', link: '/account/settings/alerts' },
     ],
   ];
-  
+
   return (
     <div className="page-template">
       <div className="drawer-container">
-        {/*이곳의 drawer에는 현재 User의 Id가 들어가면 됨*/}
-        <Drawer menulist={menulist} Id={user.Id} />
+        <Drawer menulist={menulist} id={user.Id} />
       </div>
       <div className="setting-page">
         <Switch>
           {/* <Route path="/setting/themechange" component={} /> */}
-          <Route path="/setting/group" component={GroupSettings} />
-          <Route path="/setting" component={ProfileSettings} />
+          <Route path="/account/settings/group" component={GroupSettings} />
+          <Route path="/account/settings" exact component={ProfileSettings} />
         </Switch>
       </div>
     </div>
