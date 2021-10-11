f5e13fec7f1b6ddeb1f508ccb5f22d1ad9446c1d
Woo hyeok Choi <cwooh8@gmail.com>
2021-10-11 09:09:23 +0000

    Feat: Add TabPanel to ProfileSettings
    Resolves: #33

--- frontend/components/organisms/ProfileSettings/ProfileSettings.js
+++ frontend/components/organisms/ProfileSettings/ProfileSettings.js
@@ -11,13 +11,19 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
+import TabPanel from '_molecules/TabPanel';
 
 import { validatePassword } from "_utils/validation";
 
 export default function ProfileSettings() {
   const { user } = useSelector(R.pick(['user']));
+  console.log(user);
   const { group } = useSelector(R.pick(['group']));
 
+  const [tabNumber, setTabNumber] = useState(0);
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [rank, setRank] = useState(user.rank);
@@ -26,6 +32,11 @@ export default function ProfileSettings() {
   const [email, setEmail] = useState(user.email);
   const [militaryTel, setMilitaryTel] = useState(user.tel.military);
   const [mobileTel, setMobileTel] = useState(user.tel.mobile);
+  const [profileImageUrl, setProfileImageUrl] = useState(user.profileImageUrl);
+
+  const handleChange = (event, newValue) => {
+    setTabNumber(newValue);
+  };
 
   // Password validation error states
   const [passwordMessage, setPasswordMessage] = useState("");
@@ -90,160 +101,186 @@ export default function ProfileSettings() {
     );
   };
 
-  return (
-    <Box className="profile-setting-box">
-      <div className="profile-setting-title"> 프로필 설정 </div>
-      <div className="profile-img-label"> 사진 </div>
-      <div>
-        <img
-          className="profile-img"
-          /*src={profilePic || '/images/default-profile.png'}*/
-          src={'/images/default-profile.png'}
-          alt="profile"
-        />
-      </div>
-      <Button className="profile-setting-button" variant = "contained" > 프로필 이미지 수정 </Button>
-      <FormControl fullWidth margin="normal">
-        <InputLabel htmlFor="select-rank">계급/등급</InputLabel>
-        <Select defaultValue={user.rank} id="select-rank" label="계급/등급" onChange={(event) => handleInputChange(event, setRank)}>
-          <MenuItem value="" disabled={true}>
-            선택
-          </MenuItem>
-          <ListSubheader>장교</ListSubheader>
-          {mapMenuItem([
-            "대장",
-            "중상",
-            "소장",
-            "준장",
-            "대령",
-            "중령",
-            "소령",
-            "대위",
-            "중위",
-            "소위",
-          ])}
-          <ListSubheader>부사관</ListSubheader>
-          {mapMenuItem(["준위", "원사", "상사", "중사", "하사"])}
-          <ListSubheader>용사</ListSubheader>
-          {mapMenuItem(["병장", "상병", "일병", "이등병"])}
-          <ListSubheader>군무원</ListSubheader>
-          {mapMenuItem([
-            "군무원 1급",
-            "군무원 2급",
-            "군무원 3급",
-            "군무원 4급",
-            "군무원 5급",
-            "군무원 6급",
-            "군무원 7급",
-            "군무원 8급",
-            "군무원 9급",
-            "군무원 10급",
-          ])}
-          <ListSubheader>공무원</ListSubheader>
-          {mapMenuItem([
-            "공무원 1급",
-            "공무원 2급",
-            "공무원 3급",
-            "공무원 4급",
-            "공무원 5급",
-            "공무원 6급",
-            "공무원 7급",
-            "공무원 8급",
-            "공무원 9급",
-            "공무원 10급",
-          ])}
-        </Select>
-      </FormControl>
-      <FormControl fullWidth margin="normal">
-        <InputLabel htmlFor="select-rank">계정 상태</InputLabel>
-        <Select defaultValue={user.status} id="select-status" label="계정 상태" onChange={(event) => handleInputChange(event, setStatus)}>
-          <MenuItem value="" disabled={true}>선택</MenuItem>
-          <MenuItem value={'active'}>활성</MenuItem>
-          <MenuItem value={'inactive'}>비활성</MenuItem>
-          <MenuItem value={'discharged'}>전역</MenuItem>
-        </Select>
-      </FormControl>
-      <TextField
-        fullWidth
-        id="input-password"
-        label="비밀번호"
-        type="password"
-        value={password}
-        onChange={handlePasswordChange}
-        margin="normal"
-        color={password ? (passwordValid ? "success" : "error") : ""}
-        error={password.length > 1 && !passwordValid}
-        helperText={passwordMessage}
-      />
-      <TextField
-        fullWidth
-        id="input-password-check"
-        label="비밀번호 확인"
-        type="password"
-        value={passwordConfirm}
-        onChange={handlePasswordConfirmChange}
-        margin="normal"
-        color={
-          passwordConfirm ? (passwordConfirmValid ? "success" : "error") : ""
-        }
-        error={passwordConfirm.length > 1 && !passwordConfirmValid}
-        helperText={passwordConfirmMessage}
-      />
-      <TextField
-        fullWidth
-        id="input-title"
-        label="직무명"
-        placeholder="인사 담당관, 대대장, ..."
-        value={title}
-        onChange={(event) => handleInputChange(event, setTitle)}
-        margin="normal"
-      />
-      <TextField
-        fullWidth
-        id="input-email"
-        label="군 이메일"
-        type="email"
-        placeholder="example@mnd.mil" // TODO: Add email format validation
-        value={email}
-        onChange={(event) => handleInputChange(event, setEmail)}
-        margin="normal"
-      />
-      <TextField
-        fullWidth
-        id="input-tel-military"
-        label="군 전화번호"
-        type="tel"
-        placeholder="000-0000" // TODO: Add phone number validation
-        value={militaryTel}
-        onChange={(event) => handleInputChange(event, setMilitaryTel)}
-        margin="normal"
-      />
-      <TextField
-        fullWidth
-        id="input-tel-mobile"
-        label="휴대폰 전화번호"
-        type="tel"
-        placeholder="010-0000-0000" // TODO: Add phone number validation
-        value={mobileTel}
-        onChange={(event) => handleInputChange(event, setMobileTel)}
-        margin="normal"
-      />
-      <Tooltip
-        title={isInputValid() ? "" : "필수 정보를 모두 입력해주세요"}
-        placement="bottom"
-      >
-        <span style={{ width: "100%" }}>
-          <Button
-            className="profile-setting-button"
-            fullWidth
-            variant="contained"
-            size="large"
-            disabled={!isInputValid()}
-          >
-            프로필 수정
-          </Button>
-        </span>
-      </Tooltip>
+  return (<div>
+    <Box className="tabs-container">
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs
+          value={tabNumber}
+          onChange={handleChange}
+          TabIndicatorProps={{ style: { background: 'black' } }}
+          variant="scrollable"
+          scrollButtons="auto"
+        >
+          <Tab label="프로필 설정" />
+        </Tabs>
+      </Box>
+      <TabPanel value={tabNumber} index={0}>
+        <div className="tabpanel">
+          <Box className="profile-setting-box">
+            <div className="profile-img-label"> 프로필 사진 </div>
+            <div>
+              <img
+                className="profile-img"
+                /*src={profilePic || '/images/default-profile.png'}*/
+                src={user.profileImageUrl || '/images/default-profile.png'}
+                alt="profile"
+              />
+              <TextField
+                fullWidth
+                id="input-profileImageUrl"
+                label="프로필 사진"
+                value={profileImageUrl}
+                onChange={(event) => handleInputChange(event, setProfileImageUrl)}
+                placeholder="프로필 사진의 링크를 입력해주세요"
+                margin="normal"
+              />
+            </div>
+            <FormControl fullWidth margin="normal">
+              <InputLabel htmlFor="select-rank">계급/등급</InputLabel>
+              <Select defaultValue={user.rank} id="select-rank" label="계급/등급" onChange={(event) => handleInputChange(event, setRank)}>
+                <MenuItem value="" disabled={true}>
+                  선택
+                </MenuItem>
+                <ListSubheader>장교</ListSubheader>
+                {mapMenuItem([
+                  "대장",
+                  "중상",
+                  "소장",
+                  "준장",
+                  "대령",
+                  "중령",
+                  "소령",
+                  "대위",
+                  "중위",
+                  "소위",
+                ])}
+                <ListSubheader>부사관</ListSubheader>
+                {mapMenuItem(["준위", "원사", "상사", "중사", "하사"])}
+                <ListSubheader>용사</ListSubheader>
+                {mapMenuItem(["병장", "상병", "일병", "이등병"])}
+                <ListSubheader>군무원</ListSubheader>
+                {mapMenuItem([
+                  "군무원 1급",
+                  "군무원 2급",
+                  "군무원 3급",
+                  "군무원 4급",
+                  "군무원 5급",
+                  "군무원 6급",
+                  "군무원 7급",
+                  "군무원 8급",
+                  "군무원 9급",
+                  "군무원 10급",
+                ])}
+                <ListSubheader>공무원</ListSubheader>
+                {mapMenuItem([
+                  "공무원 1급",
+                  "공무원 2급",
+                  "공무원 3급",
+                  "공무원 4급",
+                  "공무원 5급",
+                  "공무원 6급",
+                  "공무원 7급",
+                  "공무원 8급",
+                  "공무원 9급",
+                  "공무원 10급",
+                ])}
+              </Select>
+            </FormControl>
+            <FormControl fullWidth margin="normal">
+              <InputLabel htmlFor="select-rank">계정 상태</InputLabel>
+              <Select defaultValue={user.status} id="select-status" label="계정 상태" onChange={(event) => handleInputChange(event, setStatus)}>
+                <MenuItem value="" disabled={true}>선택</MenuItem>
+                <MenuItem value={'active'}>활성</MenuItem>
+                <MenuItem value={'inactive'}>비활성</MenuItem>
+                <MenuItem value={'discharged'}>전역</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField
+              fullWidth
+              id="input-password"
+              label="비밀번호"
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              margin="normal"
+              color={password ? (passwordValid ? "success" : "error") : ""}
+              error={password.length > 1 && !passwordValid}
+              helperText={passwordMessage}
+            />
+            <TextField
+              fullWidth
+              id="input-password-check"
+              label="비밀번호 확인"
+              type="password"
+              value={passwordConfirm}
+              onChange={handlePasswordConfirmChange}
+              margin="normal"
+              color={
+                passwordConfirm ? (passwordConfirmValid ? "success" : "error") : ""
+              }
+              error={passwordConfirm.length > 1 && !passwordConfirmValid}
+              helperText={passwordConfirmMessage}
+            />
+            <TextField
+              fullWidth
+              id="input-title"
+              label="직무명"
+              placeholder="인사 담당관, 대대장, ..."
+              value={title}
+              onChange={(event) => handleInputChange(event, setTitle)}
+              margin="normal"
+            />
+            <TextField
+              fullWidth
+              id="input-email"
+              label="군 이메일"
+              type="email"
+              placeholder="example@mnd.mil" // TODO: Add email format validation
+              value={email}
+              onChange={(event) => handleInputChange(event, setEmail)}
+              margin="normal"
+            />
+            <TextField
+              fullWidth
+              id="input-tel-military"
+              label="군 전화번호"
+              type="tel"
+              placeholder="000-0000" // TODO: Add phone number validation
+              value={militaryTel}
+              onChange={(event) => handleInputChange(event, setMilitaryTel)}
+              margin="normal"
+            />
+            <TextField
+              fullWidth
+              id="input-tel-mobile"
+              label="휴대폰 전화번호"
+              type="tel"
+              placeholder="010-0000-0000" // TODO: Add phone number validation
+              value={mobileTel}
+              onChange={(event) => handleInputChange(event, setMobileTel)}
+              margin="normal"
+            />
+            <Tooltip
+              title={isInputValid() ? "" : "필수 정보를 모두 입력해주세요"}
+              placement="bottom"
+            >
+              <span style={{ width: "100%" }}>
+                <Button
+                  className="profile-setting-button"
+                  fullWidth
+                  variant="contained"
+                  size="large"
+                  disabled={!isInputValid()}
+                >
+                  프로필 수정
+                </Button>
+              </span>
+            </Tooltip>
+          </Box>
+        </div>
+      </TabPanel>
     </Box>
+
+  </div>
   );
 }
