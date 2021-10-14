9a305da6efdc259bb210dceb1a80d58104d3962e
Nathan Cho <ntcho13@gmail.com>
2021-10-15 01:45:55 +0900

    Feat: Add bookmark tiggke thunks

--- frontend/store/thunks/user.js
+++ frontend/store/thunks/user.js
@@ -1,10 +1,8 @@
+import { push } from "connected-react-router";
 import { snakeToCamelCase } from "json-style-converter/es5";
 import { store as RNC } from "react-notifications-component";
-import { push } from "connected-react-router";
-
-import { getUser, putUser, putUserPassword } from "_api/user";
 import { updateUser } from "_actions/user";
-
+import { getUser, putUser, putUserPassword } from "_api/user";
 import { dispatchError } from "_utils/api";
 
 export const attemptGetUser = () => (dispatch) => {
@@ -38,6 +36,54 @@ export const attemptUpdateUser = (updatedUser) => (dispatch) => {
       RNC.addNotification({
         title: "프로필 수정",
         message: "변경사항이 저장되었습니다.",
+        type: "success",
+        container: "top-center",
+        animationIn: ["animated", "fadeInRight"],
+        animationOut: ["animated", "fadeOutRight"],
+        dismiss: {
+          duration: 5000,
+        },
+      });
+
+      return data;
+    })
+    .catch(dispatchError(dispatch));
+};
+
+export const attemptAddBookmark = (updatedUser) => (dispatch) => {
+  const currentUserId = JSON.parse(localStorage.getItem("user"))._id;
+
+  return putUser(currentUserId, updatedUser)
+    .then((data) => {
+      dispatch(updateUser(snakeToCamelCase(data)));
+
+      RNC.addNotification({
+        title: "북마크 추가",
+        message: "북마크에 추가되었습니다.",
+        type: "success",
+        container: "top-center",
+        animationIn: ["animated", "fadeInRight"],
+        animationOut: ["animated", "fadeOutRight"],
+        dismiss: {
+          duration: 5000,
+        },
+      });
+
+      return data;
+    })
+    .catch(dispatchError(dispatch));
+};
+
+export const attemptRemoveBookmark = (updatedUser) => (dispatch) => {
+  const currentUserId = JSON.parse(localStorage.getItem("user"))._id;
+
+  return putUser(currentUserId, updatedUser)
+    .then((data) => {
+      dispatch(updateUser(snakeToCamelCase(data)));
+
+      RNC.addNotification({
+        title: "북마크 제거",
+        message: "북마크에서 제거되었습니다.",
         type: "success",
         container: "top-center",
         animationIn: ["animated", "fadeInRight"],
