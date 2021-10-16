eccfafeec31277124cc63fa0b2d1504e20c2b6d9
Nathan Cho <ntcho13@gmail.com>
2021-10-16 21:47:35 +0900

    Chore: Reformat code

--- frontend/components/pages/EditorPage/EditorPage.js
+++ frontend/components/pages/EditorPage/EditorPage.js
@@ -1,10 +1,9 @@
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
 import React, { useState } from "react";
 import Editor from "_frontend/components/molecules/Editor";
-
-import Container from "@mui/material/Container";
-import TextField from "@mui/material/TextField";
-import Stack from "@mui/material/Stack";
-import Button from '@mui/material/Button'
 
 export default function EditorPage({ itemId }) {
   const [title, setTitle] = useState("");
@@ -14,23 +13,25 @@ export default function EditorPage({ itemId }) {
   const handleChange = (e) => setTitle(e.target.value);
 
   return (
-    <div>
-      <Container maxWidth="md" sx={{ py: 4 }}>
-        <Stack spacing={1}>
-          <TextField
-            fullWidth
-            id="card-title"
-            label="제목"
-            variant="outlined"
-            value={title}
-            onChange={handleChange}
-          />
-          <Editor onContentChange={html => setContent(html)} />
-          <Button variant="contained" color="primary" onClick={() => alert(content)}>
-            카드 게시
-          </Button>
-        </Stack>
-      </Container>
-    </div>
+    <Container maxWidth="md" sx={{ py: 4 }}>
+      <Stack spacing={1}>
+        <TextField
+          fullWidth
+          id="card-title"
+          label="제목"
+          variant="outlined"
+          value={title}
+          onChange={handleChange}
+        />
+        <Editor onContentChange={(html) => setContent(html)} />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => alert(content)}
+        >
+          카드 게시
+        </Button>
+      </Stack>
+    </Container>
   );
 }
