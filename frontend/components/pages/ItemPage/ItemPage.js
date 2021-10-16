d5a6e0098e6572b83541ff92f4076b33d6f39839
Nathan Cho <ntcho13@gmail.com>
2021-10-17 03:51:18 +0900

    Feat: Add action buttons to ItemPage

--- frontend/components/pages/ItemPage/ItemPage.js
+++ frontend/components/pages/ItemPage/ItemPage.js
@@ -1,20 +1,69 @@
-import { Container, Stack, Tooltip } from "@mui/material";
+import {
+  mdiAccountMultipleCheck,
+  mdiContentDuplicate,
+  mdiDelete,
+  mdiDotsVertical,
+  mdiEarth,
+  mdiFileEditOutline,
+  mdiPackageDown,
+  mdiShare,
+  mdiStar,
+  mdiStarOutline,
+  mdiUpload
+} from "@mdi/js";
+import Icon from "@mdi/react";
+import {
+  Badge,
+  Container,
+  Divider,
+  IconButton,
+  ListItemIcon,
+  Menu,
+  MenuItem,
+  Stack,
+  Tooltip
+} from "@mui/material";
 import { push } from "connected-react-router";
 import R from "ramda";
 import React, { useEffect, useState } from "react";
+import { store as RNC } from "react-notifications-component";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
+import LinkComponent from "_atoms/LinkComponent";
 import TypeIcon from "_atoms/TypeIcon";
 import BreadCrumbs from "_molecules/BreadCrumbs";
 import Editor from "_molecules/Editor";
 import Comment from "_organisms/Comment";
-import { attemptGetItem, attemptGetItemChildren } from "_thunks/item";
+import {
+  attemptArchiveItem,
+  attemptDeleteItem,
+  attemptGetItem,
+  attemptGetItemChildren,
+  attemptPublishItem
+} from "_thunks/item";
+import { attemptAddBookmark, attemptRemoveBookmark } from "_thunks/user";
 import { deepEqual } from "_utils/compare";
+
+const typeString = {
+  cabinet: "서랍",
+  document: "문서",
+  card: "카드",
+};
 
 const hierarchyLevel = {
   cabinet: 1,
   document: 2,
   card: 3,
+};
+
+const statusIcon = {
+  archived: mdiPackageDown,
+  public: mdiEarth,
+};
+
+const statusTooltipText = {
+  archived: "보관된",
+  public: "공개된",
 };
 
 export default function ItemPage() {
@@ -53,16 +102,6 @@ export default function ItemPage() {
   const cachedParentParent = useSelector(
     (state) => state.itemCache[pathArray.length > 2 ? pathArray[2] : null]
   );
-
-  // DEBUG
-  if (cachedItem == null) console.log(itemId + " wasn't found in itemCache.");
-  else console.log(itemId + " was found in itemCache: ", cachedItem);
-
-  const [loadingPath, setLoadingPath] = useState(true);
-  const [loadingChild, setLoadingChild] = useState(true);
-  const [pathObject, setPathObject] = useState({});
-  const [childObject, setChildObject] = useState({});
-  // const [item, setItem] = useState({});
 
   useEffect(() => {
     if (itemId.length != 24) {
@@ -119,10 +158,7 @@ export default function ItemPage() {
         );
       }
     } else {
-      localStorage.setItem(
-        "recents",
-        JSON.stringify([itemId])
-      );
+      localStorage.setItem("recents", JSON.stringify([itemId]));
     }
   }, [itemId]);
 
@@ -174,24 +210,139 @@ export default function ItemPage() {
     }
   }, [item]);
 
-  console.log("render; item =", item, "visible =", visible);
+  const shareItem = () => {
+    navigator.clipboard.writeText(`${location.hostname}/item/${itemId}`);
+
+    RNC.addNotification({
+      title: "클립보드에 복사됨",
+      type: "success",
+      message: "항목 링크가 클립보드에 복사되었습니다.",
+      container: "top-center",
+      animationIn: ["animated", "fadeInRight"],
+      animationOut: ["animated", "fadeOutRight"],
+      dismiss: {
+        duration: 5000,
+      },
+    });
+  };
+
+  const deleteItem = () => {
+    dispatch(attemptDeleteItem(itemId));
+    setVisible(false);
+  };
+
+  const archiveItem = () => {
+    dispatch(attemptArchiveItem(itemId));
+    setItem({ ...item, status: "archived" });
+  };
+
+  const publishItem = () => {
+    dispatch(attemptPublishItem(itemId));
+    setItem({ ...item, status: "published" });
+  };
+
+  const toggleBookmark = () => {
+    let bookmarkArray = user.bookmarks;
+
+    if (isBookmarked) {
+      // previously was bookmarked, delete from bookmark array
+      bookmarkArray = bookmarkArray.filter((elem) => elem !== itemId);
+      dispatch(attemptRemoveBookmark({ bookmarks: bookmarkArray }));
+    } else {
+      // previously wasn't bookmarked, add to bookmark array
+      bookmarkArray = [...bookmarkArray, itemId];
+      dispatch(attemptAddBookmark({ bookmarks: bookmarkArray }));
+    }
+
+    setBookmarked(!isBookmarked);
+  };
+
+  // states for menu component
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const status =
+    item == null ||
+    !item.hasOwnProperty("status") ||
+    !item.hasOwnProperty("accessGroups")
+      ? null
+      : item.status === "archived"
+      ? "archived"
+      : item.accessGroups.read === "all"
+      ? "public"
+      : null;
+
+  const isCurrentUserOwner = item ? item.owner._id === user._id : false;
+  const isCurrentUserEditor =
+    item != null && item.hasOwnProperty("accessGroups")
+      ? item.accessGroups.edit.includes(user.group)
+      : false;
 
   return visible && item != null ? (
     // <div className="content-pane">
     // <Header />
     <Container maxWidth="md" sx={{ py: 4 }}>
       <Stack spacing={1} className="item-page">
-        <div className="item-header">
-          <div className="item-title">{item.title}</div>
-          <Tooltip title="서랍" arrow>
-            <TypeIcon type={item.type} size={2} opacity={0.7} />
-          </Tooltip>
-        </div>
-        {itemParents != null ? (
-          <BreadCrumbs itemArray={[...itemParents, item]} />
-        ) : (
-          <BreadCrumbs hierarchyLevel={hierarchyLevel[item.type]} />
-        )}
+        <Stack>
+          <div className="item-header">
+            <Tooltip
+              title={
+                (status != null ? `${statusTooltipText[status]} ` : "") +
+                typeString[item.type]
+              }
+              arrow
+            >
+              <Badge
+                badgeContent={
+                  status != null ? (
+                    <Icon size={0.75} path={statusIcon[status]} opacity={0.7} />
+                  ) : (
+                    null
+                  )
+                }
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                sx={{
+                  ".MuiBadge-badge": {
+                    backgroundColor: "#eae8dc",
+                    p: "4px",
+                    mb: "4px",
+                    mr: "4px",
+                  },
+                }}
+              >
+                <TypeIcon type={item.type} size={1.5} opacity={0.7} />
+              </Badge>
+            </Tooltip>
+            <div className="item-title">{item.title}</div>
+            <Stack direction="row">
+              <Tooltip title="북마크에 추가" arrow>
+                <IconButton onClick={toggleBookmark}>
+                  <Icon
+                    size={1.25}
+                    path={isBookmarked ? mdiStar : mdiStarOutline}
+                  />
+                </IconButton>
+              </Tooltip>
+              <IconButton onClick={handleClick}>
+                <Icon size={1} path={mdiDotsVertical} />
+              </IconButton>
+            </Stack>
+          </div>
+          {itemParents != null ? (
+            <BreadCrumbs itemArray={[...itemParents, item]} />
+          ) : (
+            <BreadCrumbs hierarchyLevel={hierarchyLevel[item.type]} />
+          )}
+        </Stack>
         {item.content && <Editor content={item.content} editable={false} />}
         <div>
           <div className="outer-div">
@@ -208,6 +359,92 @@ export default function ItemPage() {
             ))}
         </div>
       </Stack>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            ml: "6px",
+            mt: 0.5,
+            "&:before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 20,
+              width: 10,
+              height: 10,
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        {isCurrentUserOwner || isCurrentUserEditor ? ( // only show edit menu to owner and editor
+          <MenuItem component={LinkComponent} to={`/item/${itemId}/edit`}>
+            <ListItemIcon>
+              <Icon path={mdiFileEditOutline} size={1} />
+            </ListItemIcon>
+            수정
+          </MenuItem>
+        ) : (
+          <div />
+        )}
+        <MenuItem component={LinkComponent} to={`/item/${itemId}/duplicate`}>
+          <ListItemIcon>
+            <Icon path={mdiContentDuplicate} size={1} />
+          </ListItemIcon>
+          복제
+        </MenuItem>
+        <MenuItem onClick={shareItem}>
+          <ListItemIcon>
+            <Icon path={mdiShare} size={1} />
+          </ListItemIcon>
+          공유
+        </MenuItem>
+        {isCurrentUserOwner ? (
+          <div>
+            <MenuItem component={LinkComponent} to={`/item/${itemId}/settings`}>
+              <ListItemIcon>
+                <Icon path={mdiAccountMultipleCheck} size={1} />
+              </ListItemIcon>
+              권한 설정
+            </MenuItem>
+            <Divider light />
+            {status !== "archived" ? (
+              <MenuItem onClick={archiveItem}>
+                <ListItemIcon>
+                  <Icon path={mdiPackageDown} size={1} />
+                </ListItemIcon>
+                보관
+              </MenuItem>
+            ) : (
+              <MenuItem onClick={publishItem}>
+                <ListItemIcon>
+                  <Icon path={mdiUpload} size={1} />
+                </ListItemIcon>
+                게시
+              </MenuItem>
+            )}
+            <MenuItem onClick={deleteItem}>
+              <ListItemIcon>
+                <Icon path={mdiDelete} size={1} />
+              </ListItemIcon>
+              삭제
+            </MenuItem>
+          </div>
+        ) : (
+          <div />
+        )}
+      </Menu>
     </Container>
   ) : (
     <div>Loading...</div>
