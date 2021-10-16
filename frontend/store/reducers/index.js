474e4f177aad17b1fa3b105883bc4572de9c35d7
Nathan Cho <ntcho13@gmail.com>
2021-10-17 01:28:23 +0900

    Remove: Remove artifacts from boilerplate
    Resolves: #184

--- frontend/store/reducers/index.js
+++ frontend/store/reducers/index.js
@@ -1,17 +1,15 @@
-import { connectRouter } from 'connected-react-router';
-import { combineReducers } from 'redux';
-import group from './group';
-import itemCache from './itemCache';
-import todos from './todos';
-import user from './user';
+import { connectRouter } from "connected-react-router";
+import { combineReducers } from "redux";
+import group from "./group";
+import itemCache from "./itemCache";
+import user from "./user";
 
-
-const createRootReducer = (history) => combineReducers({
-  router: connectRouter(history),
-  user,
-  todos,
-  group,
-  itemCache,
-});
+const createRootReducer = (history) =>
+  combineReducers({
+    router: connectRouter(history),
+    user,
+    group,
+    itemCache,
+  });
 
 export default createRootReducer;
