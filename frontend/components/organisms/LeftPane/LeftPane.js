9b342a279f89f20acab8349d879da3513544a57a
Nathan Cho <ntcho13@gmail.com>
2021-10-18 18:27:39 +0900

    Style: Reformat code

--- frontend/components/organisms/LeftPane/LeftPane.js
+++ frontend/components/organisms/LeftPane/LeftPane.js
@@ -1,29 +1,33 @@
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListSubheader from '@mui/material/ListSubheader';
-import R from 'ramda';
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import MenuItem from '_molecules/MenuItem';
-import ProfileMenu from '_molecules/ProfileMenu';
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListSubheader from "@mui/material/ListSubheader";
+import R from "ramda";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import MenuItem from "_molecules/MenuItem";
+import ProfileMenu from "_molecules/ProfileMenu";
 
-
-const dummy = [{
-  Id: 1,
-  title: '군수과',
-  type: 'cabinet',
-},
-{
-  Id: 2,
-  title: '00대대',
-  type: 'cabinet',
-}];
+const dummy = [
+  {
+    Id: 1,
+    title: "군수과",
+    type: "cabinet",
+  },
+  {
+    Id: 2,
+    title: "00대대",
+    type: "cabinet",
+  },
+];
 
 function arrayToMenuItems(array) {
   return array.map((elem, index) => (
     <ListItem disableGutters sx={{ py: 0 }} key={index}>
       <ListItemButton disableGutters sx={{ py: 0 }}>
-        <MenuItem key={elem.Id} value={{ title: elem.title, Id: elem.Id, link: `/item/${elem.Id}` }} />
+        <MenuItem
+          key={elem.Id}
+          value={{ title: elem.title, Id: elem.Id, link: `/item/${elem.Id}` }}
+        />
       </ListItemButton>
     </ListItem>
   ));
@@ -32,40 +36,46 @@ function arrayToMenuItems(array) {
 export default function LeftPane() {
   const dispatch = useDispatch();
   // if user exists, means a user has logged in, i.e. item, group items are available
-  const { user } = useSelector(R.pick(['user']));
-  const { group } = useSelector(R.pick(['group']));
-  const { userItem } = useSelector(R.pick(['userItem']));
+  const { user } = useSelector(R.pick(["user"]));
+  const { group } = useSelector(R.pick(["group"]));
 
   return (
     <div className="leftpane">
       <div className="leftpane-header">
-        <ProfileMenu name={user.name} rank={user.rank} title={user.title} division={group.name} />
+        <ProfileMenu
+          name={user.name}
+          rank={user.rank}
+          title={user.title}
+          division={group.name}
+        />
       </div>
       <div className="leftpane-menu">
         <div className="leftpane-menu1">
           <ListSubheader className="leftpane-label"> 내 서랍 </ListSubheader>
-          {arrayToMenuItems(dummy.filter((elem) => elem.type === 'cabinet'))}
+          {arrayToMenuItems(dummy.filter((elem) => elem.type === "cabinet"))}
         </div>
         <div className="leftpane-menu2">
           <ListSubheader className="leftpane-label"> 내 문서 </ListSubheader>
           <ListItem disableGutters sx={{ py: 0 }}>
             <ListItemButton disableGutters sx={{ py: 0 }}>
-              <MenuItem value={{ title: '북마크', link: '/bookmarks' }} />
+              <MenuItem value={{ title: "북마크", link: "/bookmarks" }} />
             </ListItemButton>
           </ListItem>
           <ListItem disableGutters sx={{ py: 0 }}>
             <ListItemButton disableGutters sx={{ py: 0 }}>
-              <MenuItem value={{ title: '최근에 본 문서', link: '/recents' }} />
+              <MenuItem value={{ title: "최근에 본 문서", link: "/recents" }} />
             </ListItemButton>
           </ListItem>
           <ListItem disableGutters sx={{ py: 0 }}>
             <ListItemButton disableGutters sx={{ py: 0 }}>
-              <MenuItem value={{ title: '내가 작성한 문서', link: '/account/items' }} />
+              <MenuItem
+                value={{ title: "내가 작성한 문서", link: "/account/items" }}
+              />
             </ListItemButton>
           </ListItem>
           <ListItem disableGutters sx={{ py: 0 }}>
             <ListItemButton disableGutters sx={{ py: 0 }}>
-              <MenuItem value={{ title: '임시저장한 문서', link: '/drafts' }} />
+              <MenuItem value={{ title: "임시저장한 문서", link: "/drafts" }} />
             </ListItemButton>
           </ListItem>
         </div>
@@ -73,17 +83,17 @@ export default function LeftPane() {
           <ListSubheader className="leftpane-label"> 기타 </ListSubheader>
           <ListItem disableGutters sx={{ py: 0 }}>
             <ListItemButton disableGutters sx={{ py: 0 }}>
-              <MenuItem value={{ title: '설정', link: '/setting' }} />
+              <MenuItem value={{ title: "설정", link: "/setting" }} />
             </ListItemButton>
           </ListItem>
           <ListItem disableGutters sx={{ py: 0 }}>
             <ListItemButton disableGutters sx={{ py: 0 }}>
-              <MenuItem value={{ title: '도움말', link: '/' }} />
+              <MenuItem value={{ title: "도움말", link: "/" }} />
             </ListItemButton>
           </ListItem>
           <ListItem disableGutters sx={{ py: 0 }}>
             <ListItemButton disableGutters sx={{ py: 0 }}>
-              <MenuItem value={{ title: '서비스 페이지', link: '/' }} />
+              <MenuItem value={{ title: "서비스 페이지", link: "/" }} />
             </ListItemButton>
           </ListItem>
         </div>
