e45c56d7b97fac414b548ee26cbf6225fd7575b0
Nathan Cho <ntcho13@gmail.com>
2021-10-18 19:36:28 +0900

    Fix: TypeIcon not showing error

--- frontend/components/atoms/TypeIcon/TypeIcon.js
+++ frontend/components/atoms/TypeIcon/TypeIcon.js
@@ -12,11 +12,11 @@ const icon = {
   card: mdiTextBoxOutline,
 };
 
-export default function TypeIcon({ type, ...props }) {
+export default function TypeIcon({ type, path, ...props }) {
   const isString = typeof type === "string";
   return isString ? (
     <Icon path={icon[type]} {...props} />
   ) : (
-    <Icon path={type} {...props} />
+    <Icon path={path} {...props} />
   );
 }
