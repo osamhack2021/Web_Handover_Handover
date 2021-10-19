d214574c744173d7732ace5cf86e19412426b78c
Nathan Cho <ntcho13@gmail.com>
2021-10-20 03:41:53 +0900

    Feat: Pass props to ItemListHeader

--- frontend/components/molecules/ItemListHeader/ItemListHeader.js
+++ frontend/components/molecules/ItemListHeader/ItemListHeader.js
@@ -1,9 +1,9 @@
 import React from "react";
 import TypeIcon from "_atoms/TypeIcon";
 
-export default function ItemListHeader({ title, icon, iconType }) {
+export default function ItemListHeader({ title, icon, iconType, ...props }) {
   return (
-    <div className="item-list-header">
+    <div className="item-list-header" {...props}>
       {iconType != null || icon != null ? (
         <TypeIcon type={iconType} path={icon} size={1.5} opacity={0.7} />
       ) : null}
